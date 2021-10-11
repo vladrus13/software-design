997c71429ccb23a8df8f92b7fdfdcc2e3a2065aa
Vladislav Kuznetsov <Vladislav.Kuznetsov@jetbrains.com>
2021-10-11 10:00:02 +0300

    hw-05. Fix main

--- hw-05-bridge/src/main/kotlin/Launcher.kt
+++ hw-05-bridge/src/main/kotlin/Launcher.kt
@@ -6,6 +6,7 @@ fun main(args : Array<String>) {
     } else {
         if (args.size != 4) {
             println("Usage: <path-to-file> <destination> <drawing-type> <graph-type>")
+            return
         }
         Instructor.adapt(Path.of(args[0]), Path.of(args[1]), args[2], args[3])
     }
